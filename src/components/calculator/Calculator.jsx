6e8ba9ad37d13d2f582988ed81fdfda631cc336c
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2025-02-09 21:30:21 +1000

    added local storage and adding and delete airplanes

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,32 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./calculator.css";
-
-const aircraftArray = {
-  C510: {
-    name: "C510",
-    fuelEconomy: 0.38043,
-    fuelCapacity: 386,
-    pax: 5,
-    payload: 500,
-    seats: 5,
-  },
-  A320: {
-    name: "A320",
-    fuelEconomy: 2.37575758,
-    fuelCapacity: 7840,
-    pax: 148,
-    payload: 30279,
-    seats: 150,
-  },
-  B737800: {
-    name: "B737800",
-    fuelEconomy: 4.32244009,
-    fuelCapacity: 6875,
-    pax: 160,
-    payload: 28389,
-    seats: 162,
-  },
-};
 
 const styles = {
   grey: {
@@ -49,9 +22,54 @@ const Calculator = () => {
   const [fuel, setFuel] = useState(0);
   const [miles, setMiles] = useState(0);
   const [time, setTime] = useState(0);
-  const [aircraft, setAircraft] = useState(aircraftArray.C510);
+  const [aircraft, setAircraft] = useState(null);
   const [warning, setWarning] = useState("");
   const [mode, setMode] = useState("miles");
+  const [showAddAircraftForm, setShowAddAircraftForm] = useState(false);
+  const [aircraftArray, setAircraftArray] = useState(() => {
+    const savedAircraft = localStorage.getItem("aircraftArray");
+    return savedAircraft
+      ? JSON.parse(savedAircraft)
+      : {
+          C510: {
+            name: "C510",
+            fuelEconomy: 0.38043,
+            fuelCapacity: 386,
+            pax: 5,
+            payload: 500,
+            seats: 5,
+          },
+          A320: {
+            name: "A320",
+            fuelEconomy: 2.37575758,
+            fuelCapacity: 7840,
+            pax: 148,
+            payload: 30279,
+            seats: 150,
+          },
+          B737800: {
+            name: "B737800",
+            fuelEconomy: 4.32244009,
+            fuelCapacity: 6875,
+            pax: 160,
+            payload: 28389,
+            seats: 162,
+          },
+        };
+  });
+
+  const [newAircraft, setNewAircraft] = useState({
+    name: "",
+    fuelEconomy: "",
+    fuelCapacity: "",
+    pax: "",
+    payload: "",
+    seats: "",
+  });
+
+  useEffect(() => {
+    localStorage.setItem("aircraftArray", JSON.stringify(aircraftArray));
+  }, [aircraftArray]);
 
   function calculate() {
     if (mode === "kilos") {
@@ -101,6 +119,42 @@ const Calculator = () => {
     event.preventDefault();
   }
 
+  function handleAddAircraft(newAircraft) {
+    setAircraftArray((prevArray) => ({
+      ...prevArray,
+      [newAircraft.name]: newAircraft,
+    }));
+  }
+
+  function handleDeleteAircraft(name) {
+    setAircraftArray((prevArray) => {
+      const newArray = { ...prevArray };
+      delete newArray[name];
+      return newArray;
+    });
+  }
+
+  function handleNewAircraftChange(event) {
+    const { name, value } = event.target;
+    setNewAircraft((prevAircraft) => ({
+      ...prevAircraft,
+      [name]: value,
+    }));
+  }
+
+  function handleAddAircraftSubmit(event) {
+    event.preventDefault();
+    handleAddAircraft(newAircraft);
+    setNewAircraft({
+      name: "",
+      fuelEconomy: "",
+      fuelCapacity: "",
+      pax: "",
+      payload: "",
+      seats: "",
+    });
+  }
+
   return (
     <div className="card">
       <form className="box" id="box" onSubmit={handleSubmit}>
@@ -146,13 +200,11 @@ const Calculator = () => {
           </label>
           <div className="select">
             <select name="aircraft" onChange={handleAircraftChange}>
-              <option value={JSON.stringify(aircraftArray.C510)}>
-                Cessna 510 Mustang
-              </option>
-              <option value={JSON.stringify(aircraftArray.A320)}>A320</option>
-              <option value={JSON.stringify(aircraftArray.B737800)}>
-                B737800
-              </option>
+              {Object.values(aircraftArray).map((aircraft) => (
+                <option key={aircraft.name} value={JSON.stringify(aircraft)}>
+                  {aircraft.name}
+                </option>
+              ))}
             </select>
           </div>
         </div>
@@ -167,7 +219,105 @@ const Calculator = () => {
         </div>
       </form>
       <br />
-
+      <div className="box">
+        <button
+          className="dropdown button"
+          onClick={() =>
+            setShowAddAircraftForm(
+              (prevShowAddAircraftForm) => !prevShowAddAircraftForm
+            )
+          }
+        >
+          Add Aircraft
+        </button>
+        {showAddAircraftForm && (
+          <form onSubmit={handleAddAircraftSubmit}>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  name="name"
+                  value={newAircraft.name}
+                  onChange={handleNewAircraftChange}
+                  required
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Fuel Economy</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="number"
+                  name="fuelEconomy"
+                  value={newAircraft.fuelEconomy}
+                  onChange={handleNewAircraftChange}
+                  required
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Fuel Capacity</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="number"
+                  name="fuelCapacity"
+                  value={newAircraft.fuelCapacity}
+                  onChange={handleNewAircraftChange}
+                  required
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Pax</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="number"
+                  name="pax"
+                  value={newAircraft.pax}
+                  onChange={handleNewAircraftChange}
+                  required
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Payload</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="number"
+                  name="payload"
+                  value={newAircraft.payload}
+                  onChange={handleNewAircraftChange}
+                  required
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Seats</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="number"
+                  name="seats"
+                  value={newAircraft.seats}
+                  onChange={handleNewAircraftChange}
+                  required
+                />
+              </div>
+            </div>
+            <div className="control">
+              <button className="button is-primary" type="submit">
+                Add Aircraft
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
       <div>
         <table className="hover">
           <thead>
@@ -178,6 +328,7 @@ const Calculator = () => {
               <th>Fuel Capacity</th>
               <th>Total NM</th>
               <th>Payload</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
@@ -191,6 +342,14 @@ const Calculator = () => {
                   {Math.floor(aircraft.fuelCapacity / aircraft.fuelEconomy)}
                 </td>
                 <td>{aircraft.payload}</td>
+                <td>
+                  <button
+                    className="alert button"
+                    onClick={() => handleDeleteAircraft(aircraft.name)}
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
